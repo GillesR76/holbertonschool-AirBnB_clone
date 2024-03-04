5bf61e6f44f9d0273b0065c93e3f8f708f920f12
GillesR76 <gilles-richard76@hotmail.com>
2024-03-04 12:58:43 +0100

    adding the new classes

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""
+Class State that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class State that inherits from BaseModel
+    Public class Attribute:
+        name(string): name of the State
+        """
+    name = ""
