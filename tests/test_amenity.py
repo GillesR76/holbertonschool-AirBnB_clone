76d320def90644a9f1c67c655d19712e7c877aae
GillesR76 <gilles-richard76@hotmail.com>
2024-03-08 11:17:30 +0100

    moving test files to tests directory for the checker

---
+++ tests/test_amenity.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+
+import unittest
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    def setUp(self):
+        self.amenity = Amenity()
+
+    def test_instance(self):
+        self.assertIsInstance(self.amenity, Amenity)
+        self.assertTrue(hasattr(self.amenity, "id"))
+        self.assertTrue(hasattr(self.amenity, "created_at"))
+        self.assertTrue(hasattr(self.amenity, "updated_at"))
+        self.assertTrue(hasattr(self.amenity, "name"))
+        self.assertEqual(self.amenity.name, "")
+
+    def test_str(self):
+        expected_output = f"[Amenity] ({self.amenity.id}) {self.amenity.__dict__}"
+        self.assertEqual(str(self.amenity), expected_output)
+
+    def test_save(self):
+        original_updated_at = self.amenity.updated_at
+        self.amenity.save()
+        new_updated_at = self.amenity.updated_at
+        self.assertNotEqual(original_updated_at, new_updated_at)
+
+        new_amenity = Amenity()
+        new_amenity.save()
+        with open('file.json', 'r') as file:
+            self.assertIn("Amenity." + new_amenity.id, file.read())
+
+    def test_to_dict(self):
+        amenity_dict = self.amenity.to_dict()
+        self.assertEqual(amenity_dict["__class__"], "Amenity")
+        self.assertEqual(amenity_dict["id"], self.amenity.id)
+        self.assertEqual(amenity_dict["created_at"],
+                         self.amenity.created_at.isoformat())
+        self.assertEqual(amenity_dict["updated_at"],
+                         self.amenity.updated_at.isoformat())
